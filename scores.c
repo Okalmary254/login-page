e7eedeb8cee0e453cd37f984dcef291e8fc3a058
John Mary <okalmohn@proton.me>
2025-01-12 10:48:38 +0300

    Updated scores.c

--- scores.c
+++ scores.c
@@ -13,6 +13,7 @@ int main() {
     struct Student students[MAX_STUDENTS] = {
         {"P05/00023/2023", "Joy Wachira", 56},
         {"P05/00124/2023", "Nancy Kwamboka", 90}
+        {"PO5/0022/2023", "Jael Gracia", 68}
     };
 
     printf("ADMISSION NUMBER\tNAME\t\t\tSCORE\n");
